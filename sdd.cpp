bbd109ae4f2b07b9071d638585a4b2cca0442dce
DSAcomp-8 <qogambaev001@gmail.com>
2022-06-20 19:26:24 +0500

    Hello

---
+++ sdd.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DSAcomp-8 on 20.06.2022.
+//
+
+#include "sdd.h"
---
+++ sdd.h
@@ -0,0 +1,16 @@
+//
+// Created by DSAcomp-8 on 20.06.2022.
+//
+
+#ifndef GOOGLEMAP_SDD_H
+#define GOOGLEMAP_SDD_H
+
+
+
+class sdd {
+
+};
+
+
+
+#endif //GOOGLEMAP_SDD_H
